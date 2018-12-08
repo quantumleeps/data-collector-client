d6de40503c2c9f33c9c283bc6c30ff1dd924cc2f
quantumleeps <dan.leeper@gmail.com>
2018-12-08 18:02:29 -0500

    Renaming of splash screen

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -64,7 +64,7 @@ export default class Home extends Component {
   renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
+        <h1>DataCollector</h1>
         <p>A simple data collection app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
