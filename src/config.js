ef8041ce9b098d9253c8d2d75a2d039b76349c66
quantumleeps <dan.leeper@gmail.com>
2018-12-10 13:57:49 -0500

    Updating config file to current

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ export default {
   MAX_ATTACHMENT_SIZE: 15000000,
   s3: {
     REGION: "us-east-1",
-    BUCKET: "app-collector-api-dev-attachmentsbucket-p4xpydpjo1ov"
+    BUCKET: "app-collector-api-dev-attachmentsbucket-7m690qjdhtzm"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://ko598eemce.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://okfsmd5q74.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_fdLaTwPuh",
-    APP_CLIENT_ID: "4ii64d8vghaggi3s5d5iv1antr",
-    IDENTITY_POOL_ID: "us-east-1:0a7920cf-008b-400e-8b5d-99d898fe6232"
+    USER_POOL_ID: "us-east-1_61ekbBadf",
+    APP_CLIENT_ID: "6ivm0ec54sf8qkh1o5sad0rpm7",
+    IDENTITY_POOL_ID: "us-east-1:746be8b6-6fb3-4bdd-a793-23b0d4cdb927"
   }
 };
